8444b3a3c49071eefc69ee8dc15390fc3d705922
Daniel Soto <dasg.1993@gmail.com>
2023-08-01 17:13:55 -0500

    [feature] - create stack auth and implement in root navigator

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,1 +1,2 @@
 export const URL_BASE_FIREBASE_REALTIME_DATABASE = '';
+export const FIREBASE_API_KEY = '';
---
+++ src/screens/auth/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+const Auth = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Auth</Text>
+    </View>
+  );
+};
+
+export default Auth;
