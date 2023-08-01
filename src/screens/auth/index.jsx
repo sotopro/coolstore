7089d11098c377a1d32d72d21e892f943af7b776
Daniel Soto <dasg.1993@gmail.com>
2023-08-01 17:30:11 -0500

    [feature] - create ui layout for auth screen

--- src/screens/auth/index.jsx
+++ src/screens/auth/index.jsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
+import { COLORS } from '../../themes';
 
 const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const headerTitle = isLogin ? 'Login' : 'Register';
+  const buttonTitle = isLogin ? 'Login' : 'Register';
+  const messageText = isLogin ? 'Need an account?' : 'Already have an account?';
   return (
     <View style={styles.container}>
-      <Text>Auth</Text>
+      <View style={styles.content}>
+        <Text style={styles.header}>{headerTitle}</Text>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="email@domain.com"
+          placeholderTextColor={COLORS.gray}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="*********"
+          placeholderTextColor={COLORS.gray}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+          onChangeText={() => {}}
+        />
+        <View style={styles.linkContainer}>
+          <TouchableOpacity style={styles.link} onPress={() => setIsLogin(!isLogin)}>
+            <Text style={styles.linkText}>{messageText}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>{buttonTitle}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/auth/styles.js
+++ src/screens/auth/styles.js
@@ -1,9 +1,81 @@
 import { StyleSheet } from 'react-native';
 
+import { COLORS, FONTS } from '../../themes';
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.background,
+  },
+  content: {
+    width: '80%',
+    maxWidth: 400,
+    padding: 15,
+    margin: 15,
+    backgroundColor: COLORS.white,
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    borderRadius: 5,
+  },
+  header: {
+    fontFamily: FONTS.medium,
+    fontSize: 16,
+    textAlign: 'center',
+    color: COLORS.text,
+    paddingVertical: 10,
+  },
+  label: {
+    fontFamily: FONTS.light,
+    fontSize: 14,
+    color: COLORS.text,
+  },
+  input: {
+    height: 45,
+    borderBottomColor: COLORS.primary,
+    borderBottomWidth: 1,
+    width: '90%',
+    fontFamily: FONTS.regular,
+    marginBottom: 5,
+  },
+  linkContainer: {
     justifyContent: 'center',
     alignItems: 'center',
+    paddingVertical: 10,
+  },
+  link: {},
+  linkText: {
+    fontFamily: FONTS.medium,
+    fontSize: 14,
+    textAlign: 'center',
+    color: COLORS.primary,
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  button: {
+    backgroundColor: COLORS.secodary,
+    width: 200,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontFamily: FONTS.bold,
+    fontSize: 14,
+    textAlign: 'center',
+    color: COLORS.white,
   },
 });
